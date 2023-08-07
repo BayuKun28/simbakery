6ba3f7f94838129e7a12b9832e532890e25bcf31
Nurdin Ardi Nugroho <ardipethetdoplang@gmail.com>
2023-08-07 21:23:52 +0700

    ketinggalan bos

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<!-- Mirrored from demos.adminmart.com/premium/bootstrap/modernize-bootstrap/package/html/main/index5.html by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 23 Apr 2023 13:01:21 GMT -->
+<head>
+    <!--  Title -->
+    <title>Mordenize</title>
+    <!--  Required Meta Tag -->
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="handheldfriendly" content="true" />
+    <meta name="MobileOptimized" content="width" />
+    <meta name="description" content="Mordenize" />
+    <meta name="author" content="" />
+    <meta name="keywords" content="Mordenize" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!--  Favicon -->
+    <link rel="shortcut icon" type="image/png" href="{{ asset('modern/') }}/dist/images/logos/favicon.png" />
+    <!-- Core Css -->
+    <link  id="themeColors"  rel="stylesheet" href="{{ asset('modern/') }}/dist/css/style.min.css" />
+  </head>
+  <body>
